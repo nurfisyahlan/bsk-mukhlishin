4b9d7ae06b1fac34dae66240692897ceabf31e46
nurfisyahlan <nurfiisyahlan@gmail.com>
2025-08-08 01:13:33 +0700

    membuat web.php baru

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
-<?php
-
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\KeluargaController;
+use App\Http\Controllers\IuranController;
+use App\Http\Controllers\KasController;
+use App\Http\Controllers\KematianController;
+use App\Http\Controllers\InventarisController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,10 +14,27 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+// Semua route di bawah hanya bisa diakses jika sudah login
+Route::middleware(['auth'])->group(function () {
+    // PROFILE
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // KEPALA KELUARGA & ANGGOTA KELUARGA
+    Route::resource('/keluarga', KeluargaController::class);
+
+    // IURAN BULANAN
+    Route::resource('/iuran', IuranController::class);
+
+    // KAS (PEMASUKAN & PENGELUARAN)
+    Route::resource('/kas', KasController::class);
+
+    // PENCATATAN KEMATIAN & SANTUNAN
+    Route::resource('/kematian', KematianController::class);
+
+    // INVENTARIS (PERLENGKAPAN JENAZAH & TAKZIAH)
+    Route::resource('/inventaris', InventarisController::class);
 });
 
 require __DIR__.'/auth.php';
