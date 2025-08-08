91ac01658189d35e16ef3998a83ef6200f598c8b
nurfisyahlan <nurfiisyahlan@gmail.com>
2025-08-09 01:56:15 +0700

    menyusun ulang struktur baru

--- resources/views/lainnya.blade.php
+++ resources/views/lainnya.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Dashboard</title>
+    <title>Lainnya</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="p-5">
 
-    <h1 class="mb-4">Menu Dashboard</h1>
+    <h1 class="mb-4">Menu Lainnya</h1>
 
     <div class="d-flex flex-wrap gap-3">
         <a href="/monitoring-pembayaran" class="btn btn-primary btn-lg">Monitoring Pembayaran</a>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/monitoring-pembayaran', function () {
+    return view('monitoring-pembayaran');
+});
+
+Route::get('/inventaris', function () {
+    return view('inventaris');
+});
+
+Route::get('/keluarga', function () {
+    return view('keluarga');
+});
+
+Route::get('/saldo', function () {
+    return view('saldo');
+});
+
+Route::get('/lainnya', function () {
+    return view('lainnya');
+});
