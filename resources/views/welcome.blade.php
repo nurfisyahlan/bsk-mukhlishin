eb07d94a3608b3d5cba2496997473554cea4f88e
nurfisyahlan <nurfiisyahlan@gmail.com>
2025-08-13 13:44:04 +0700

    mengganti tulisan di navbar menjadi putih

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,7 +31,7 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-success shadow-sm">
             <div class="container-fluid">
                 <button class="btn btn-outline-light me-2" id="sidebarToggle">☰</button>
-                <a class="navbar-brand fw-bold" href="#">Badan Sosial Kematian</a>
+                <a class="navbar-brand fw-bold text-light" href="#">Badan Sosial Kematian</a>
             </div>
         </nav>
 
