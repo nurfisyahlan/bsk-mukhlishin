1d847d7ec683602ee30a67eecb4f7ab8984c6d45
nurfisyahlan <nurfiisyahlan@gmail.com>
2025-08-15 09:56:15 +0700

    kondisikan footer agar tidak tampil di dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,6 +22,9 @@
     </div>
 
     @include('layouts.footer')
+    @if (!Request::is('/'))
+        @include('footer')
+    @endif
 
     {{-- Bootstrap JS --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
