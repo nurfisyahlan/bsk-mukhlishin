d8069356544c560b43ce8c7911175869d9e77b60
nurfisyahlan <nurfiisyahlan@gmail.com>
2025-08-16 11:12:08 +0700

    mengondisikan tombol aktif footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,14 +1,20 @@
 <footer class="footer-menu py-2 mt-4">
     <div class="d-flex justify-content-center gap-3">
-        <a href="/monitoring-pembayaran" class="menu-btn active">
+        <a href="/monitoring-pembayaran" 
+        class="menu-btn {{ Request::is('monitoring-pembayaran') ? 'active' : '' }}">
             <span class="icon">📡</span> Pemantauan Pembayaran
         </a>
-        <a href="/saldo" class="menu-btn">
+
+        <a href="/saldo" 
+        class="menu-btn {{ Request::is('saldo') ? 'active' : '' }}">
             <span class="icon">📷</span> Laporan Keuangan
         </a>
-        <a href="/inventaris" class="menu-btn">
+
+        <a href="/inventaris" 
+        class="menu-btn {{ Request::is('inventaris') ? 'active' : '' }}">
             <span class="icon">📋</span> Daftar Inventaris
         </a>
+
         <a href="#" class="menu-btn">
             <span class="icon">➕</span> Lainnya
         </a>
