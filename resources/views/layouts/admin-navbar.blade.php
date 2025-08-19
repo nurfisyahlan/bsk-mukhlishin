3a9979de556089913b7e8a491935c3a88863c003
nurfisyahlan <nurfiisyahlan@gmail.com>
2025-08-19 17:57:23 +0700

    membuat class active sidebar dan mengganti warna navbar

--- resources/views/layouts/admin-navbar.blade.php
+++ resources/views/layouts/admin-navbar.blade.php
@@ -1,5 +1,5 @@
 <!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-light bg-success shadow-sm">
+<nav class="navbar navbar-expand-lg navbar-light shadow-sm navbar-custom">
     <div class="container-fluid">
         <button class="btn btn-outline-light me-2" id="sidebarToggle">☰</button>
         <a class="navbar-brand fw-bold text-light" href="#">Badan Sosial Kematian</a>
--- resources/views/layouts/admin-sidebar.blade.php
+++ resources/views/layouts/admin-sidebar.blade.php
@@ -3,13 +3,27 @@
     <div class="p-2">
         <h4 class="text-white mt-2 text-center">BSK Dashboard</h4>
         <nav class="nav flex-column pt-3">
-            <a class="nav-link" href="admin-dashboard">Beranda</a>
-            <a class="nav-link" href="admin-monitoring-pembayaran">Monitoring Pembayaran</a>
-            <a class="nav-link" href="admin-keluarga">Keluarga</a>
-            <a class="nav-link" href="admin-saldo">Saldo</a>
-            <a class="nav-link" href="admin-inventaris">Inventaris</a>
-            <a class="nav-link" href="admin-kematian">Kematian</a>
-            <a class="nav-link" href="admin-pengumuman">Pengumuman</a>
+            <a class="nav-link {{ Request::is('admin-dashboard') ? 'active' : '' }}" href="{{ url('admin-dashboard') }}">
+                Beranda
+            </a>
+            <a class="nav-link {{ Request::is('admin-monitoring-pembayaran') ? 'active' : '' }}" href="{{ url('admin-monitoring-pembayaran') }}">
+                Monitoring Pembayaran
+            </a>
+            <a class="nav-link {{ Request::is('admin-keluarga') ? 'active' : '' }}" href="{{ url('admin-keluarga') }}">
+                Keluarga
+            </a>
+            <a class="nav-link {{ Request::is('admin-saldo') ? 'active' : '' }}" href="{{ url('admin-saldo') }}">
+                Saldo
+            </a>
+            <a class="nav-link {{ Request::is('admin-inventaris') ? 'active' : '' }}" href="{{ url('admin-inventaris') }}">
+                Inventaris
+            </a>
+            <a class="nav-link {{ Request::is('admin-kematian') ? 'active' : '' }}" href="{{ url('admin-kematian') }}">
+                Kematian
+            </a>
+            <a class="nav-link {{ Request::is('admin-pengumuman') ? 'active' : '' }}" href="{{ url('admin-pengumuman') }}">
+                Pengumuman
+            </a>
         </nav>
     </div>
 </div>
